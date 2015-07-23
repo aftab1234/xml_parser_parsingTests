b8acee630625499612a06697458c6bd411c04297
Mohammad Aftab Sabir <maftabsabir@gmail.com>
2015-07-23 15:11:14 +0530

    a

---
+++ xml_parser_parsingTests.m
@@ -0,0 +1,34 @@
+//
+//  xml_parser_parsingTests.m
+//  xml_parser_parsingTests
+//
+//  Created by Sam on 19/02/15.
+//  Copyright (c) 2015 Bets. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xml_parser_parsingTests : XCTestCase
+
+@end
+
+@implementation xml_parser_parsingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
